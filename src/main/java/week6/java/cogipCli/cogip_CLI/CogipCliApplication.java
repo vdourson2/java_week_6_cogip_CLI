5f8a7efeeeb92008cea13aec9bb3469f9859b5ac
Virginie Dourson <virginiedourson@gmail.com>
2024-01-26 12:06:19 +0100

    Init project

---
+++ src/main/java/week6/java/cogipCli/cogip_CLI/CogipCliApplication.java
@@ -0,0 +1,13 @@
+package week6.java.cogipCli.cogip_CLI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CogipCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CogipCliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/week6/java/cogipCli/cogip_CLI/CogipCliApplicationTests.java
@@ -0,0 +1,13 @@
+package week6.java.cogipCli.cogip_CLI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CogipCliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
