06d774fe0060fe5b71621aa7026d2aeb4d9a9996
Virginie Dourson <virginiedourson@gmail.com>
2024-01-29 12:11:52 +0100

    listCompanies Json

--- src/main/java/week6/java/cogipCli/cogip_CLI/command/CompanyCommand.java
+++ src/main/java/week6/java/cogipCli/cogip_CLI/command/CompanyCommand.java
@@ -1,15 +1,40 @@
 package week6.java.cogipCli.cogip_CLI.command;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import lombok.Getter;
+import lombok.Setter;
 
 @ShellComponent
+@Getter
+@Setter
 public class CompanyCommand {
 	
-	@ShellMethod(key="hello", value="Say hello")
-	public String Hello(@ShellOption (defaultValue="World") String arg) {
-		return "Hello, " + arg;
+	private RestTemplate restTemplate;
+	
+	@Autowired
+	public CompanyCommand(RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
+	
+	//This command list the companies with essential information: name, VAT number, associated invoices, and contacts.
+	@ShellMethod(key="companies", value="Company listing with essential information: name, VAT number, associated invoices, and contacts")
+	public String listCompanies(@ShellOption (defaultValue = "false") boolean pretty) {
+		String url = "http://localhost:8080/company";
+		
+		// Fetch JSON response as String wrapped in ResponseEntity
+        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+        String productsJson = response.getBody();
+        
+        return productsJson;
 	}
 
+	
+	
 }
