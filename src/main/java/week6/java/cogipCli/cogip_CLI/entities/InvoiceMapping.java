106cbcfc50b16352b473813a271f9879f9a00b81
Valentin-Lefort <lefortvalentin94@gmail.com>
2024-02-02 13:56:35 +0100

    invoice command

--- src/main/java/week6/java/cogipCli/cogip_CLI/entities/InvoiceMapping.java
+++ src/main/java/week6/java/cogipCli/cogip_CLI/entities/InvoiceMapping.java
@@ -11,32 +11,38 @@ public class InvoiceMapping {
     @Getter
     private Short id;
     @Getter
-    private String timestamp;
-    @Getter
     private int company_id;
     private Object company;
     @Getter
     private int contact_id;
     private Object contact;
-
+    @Getter
+    private String timestamp;
+    
+    public InvoiceMapping(int id){
+        this.id = (short) id;
+    }
     public void setCompany(Object company) {
         this.company = company;
         unpackedNested();
     }
-
+    
     public void setContact(Object contact) {
         this.contact = contact;
         unpackedNested();
     }
-
-    @JsonProperty("contacts")
+    
+    public InvoiceMapping() {
+    }
+    @SuppressWarnings("unchecked")
+    @JsonProperty("invoice")
     public void unpackedNested() {
         if (company == null) {
             System.out.println("Company object is null");
         }
         else if (!(company instanceof  Map)){
             if (company instanceof Integer){
-                company_id = (int) company;
+                this.company_id = (int) company;
             }
             else{
                 System.out.println("Not Mapped");
@@ -44,23 +50,22 @@ public class InvoiceMapping {
         }
         else{
             Map<String, Object> companyMap = (Map<String, Object>) company;
-            company_id = (int) companyMap.get("id");
+            this.company_id = (int) companyMap.get("id");
         }
-
         if (contact == null) {
             System.out.println("Contact object is null");
         }
         else if (!(contact instanceof  Map)){
             if (contact instanceof Integer){
-                contact = (int) contact;
+                this.contact_id = (int) contact;
             }
             else{
                 System.out.println("Not Mapped");
             }
         }
         else{
-            Map<String, Object> companyMap = (Map<String, Object>) contact;
-            contact = (int) companyMap.get("id");
+            Map<String, Object> contactMap = (Map<String, Object>) contact;
+            this.contact_id = (int) contactMap.get("id");
         }
     }
 }
